a2a94b05cf4b303bc535d1add42330e3e02d2529
Nikolai Pustovoit <nikolaikop@gmail.com>
2021-11-11 15:24:11 -0700

    search and delete

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Posts;
+
+public class PostManager {
+
+    private Posts[] posts;
+
+    public Posts[] search(int ownerId, String domain, String query, int ownersOnly, int count, int offset) {
+        return null;
+    }
+    public void Delete(int ownerId, int postId) {
+    }
+}
